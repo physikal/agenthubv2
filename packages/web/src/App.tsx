1061b7cfc93a833ff7a5c4b2bc452246a1f56c22
physikal <boody@physikal.com>
2026-05-06 10:09:09 -0700

    feat(web): mount TlsCard on Settings + MigrationBanner in Layout

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -11,13 +11,16 @@ import { Secrets } from "./pages/Secrets.tsx";
 import { Deployments } from "./pages/Deployments.tsx";
 import { Login } from "./pages/Login.tsx";
 import { useAuthStore } from "./stores/auth.ts";
+import { MigrationBanner } from "./components/tls/MigrationBanner.js";
 
 function Layout() {
   return (
-    <div className="flex h-screen overflow-hidden">
-      <Sidebar />
-      <main className="flex-1 flex flex-col min-h-0 min-w-0">
-        <Routes>
+    <div className="flex h-screen overflow-hidden flex-col">
+      <MigrationBanner />
+      <div className="flex flex-1 min-h-0 overflow-hidden">
+        <Sidebar />
+        <main className="flex-1 flex flex-col min-h-0 min-w-0">
+          <Routes>
           <Route path="/" element={<Sessions />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="/deployments" element={<Deployments />} />
@@ -27,7 +30,8 @@ function Layout() {
           <Route path="/secrets" element={<Secrets />} />
           <Route path="/admin/users" element={<Admin />} />
         </Routes>
-      </main>
+        </main>
+      </div>
     </div>
   );
 }
